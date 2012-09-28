dd7c30d02b70f2c144ff8f86a1a9e2b0c8f765cd
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-09-28 18:05:33 +0200

    Created Transport_Room_init()

--- js/index.js
+++ js/index.js
@@ -13,6 +13,72 @@ function randomString()
 	return randomstring;
 }
 
+function Transport_Room_init(transport)
+{
+    transport.addEventListener('joiner.success', function()
+    {
+        transport.addEventListener('peer.connected', function(socket_id)
+        {
+            ui_peerstate("Peer connected!");
+
+            db.sharepoints_getAll(null, transport._send_files_list)
+
+            info(socket_id + " joined!");
+        })
+
+        transport.addEventListener('peer.disconnected', function(data)
+        {
+            ui_peerstate("Peer disconnected.");
+        })
+
+        // Add connection methods to host
+        Host_onconnect(signaling, host, db)
+
+        function _updatefiles(filelist)
+        {
+            transport._send_files_list(filelist)
+
+            ui_updatefiles_host(filelist)
+        }
+
+        db.sharepoints_getAll(null, function(filelist)
+        {
+            _updatefiles(filelist)
+
+            // Restard downloads
+            for(var i = 0, file; file = filelist[i]; i++)
+                if(file.bitmap)
+                    transport.emit('transfer.query',
+                                    file.name, getRandom(file.bitmap))
+        })
+
+        ui_onopen()
+
+        ui_ready_fileschange(function(filelist)
+        {
+            // Loop through the FileList and append files to list.
+            for(var i = 0, file; file = filelist[i]; i++)
+                db.sharepoints_add(file)
+
+            //signaling._send_files_list(filelist)   // Send just new files
+
+            db.sharepoints_getAll(null, _updatefiles)
+        })
+
+        ui_ready_transferbegin(transport._transferbegin)
+    })
+
+    transport.addEventListener('joiner.error', function(type)
+    {
+        switch(type)
+        {
+            case 'room full':
+                warning("This connection is full. Please try later.");
+        }
+    })
+}
+
+
 window.addEventListener("load", function()
 {
 	DB_init(function(db)
@@ -26,72 +92,13 @@ window.addEventListener("load", function()
         var room = window.location.hash.substring(1)
 
         // Load websocket connection after IndexedDB is ready
-        Transport_init(new WebSocket('wss://localhost:8001'), room,
+        Transport_init(new WebSocket('wss://localhost:8001'),
         function(signaling)
         {
             // Apply signaling "interface" events and functions to transport
             Transport_Signaling_init(signaling)
 
-            signaling.addEventListener('joiner.success', function()
-            {
-			    signaling.addEventListener('peer.connected', function(socket_id)
-			    {
-			        ui_peerstate("Peer connected!");
-
-			        db.sharepoints_getAll(null, signaling._send_files_list)
-
-			        info(socket_id + " joined!");
-			    })
-
-			    signaling.addEventListener('peer.disconnected', function(data)
-			    {
-			        ui_peerstate("Peer disconnected.");
-			    })
-
-                // Add connection methods to host
-                Host_onconnect(signaling, host, db)
-
-                function _updatefiles(filelist)
-                {
-                    signaling._send_files_list(filelist)
-
-                    ui_updatefiles_host(filelist)
-                }
-
-                db.sharepoints_getAll(null, function(filelist)
-                {
-                    _updatefiles(filelist)
-
-                    // Restard downloads
-                    for(var i = 0, file; file = filelist[i]; i++)
-                        if(file.bitmap)
-                            signaling.emit('transfer.query',
-                                            file.name, getRandom(file.bitmap))
-                })
-
-                ui_onopen()
-
-                ui_ready_fileschange(function(filelist)
-                {
-                    // Loop through the FileList and append files to list.
-                    for(var i = 0, file; file = filelist[i]; i++)
-                        db.sharepoints_add(file)
-
-                    //signaling._send_files_list(filelist)   // Send just new files
-
-                    db.sharepoints_getAll(null, _updatefiles)
-                })
-
-                ui_ready_transferbegin(host._transferbegin)
-            })
-	        signaling.addEventListener('joiner.error', function(type)
-            {
-                switch(type)
-                {
-                    case 'room full':
-                        warning("This connection is full. Please try later.");
-                }
-            })
+            Transport_Room_init(signaling)
 
 	        signaling.emit('joiner', room);
         })
